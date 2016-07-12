f3d1f6610777b334503d9d1d50eafe30f8ed8e12
zshandilya <zubin_shandilya@yahoo.com>
2016-07-13 01:16:42 +0530

    Assignment 1 for Java Session 1

---
+++ Assignment1.java
@@ -0,0 +1,17 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+		int Num1 = 23;
+		float fNum = 7.8f;
+		int Sum = (int) (fNum+Num1);
+		
+		System.out.println("First Number: " +Num1);
+		System.out.println("Second Number: " +fNum);
+		System.out.println("Sum of the two numbers: " +(fNum+Num1));
+		System.out.println("Rounded Value: " +Sum);
+
+	}
+
+}
